84a19661a595fa2e6fe8eb5c733741b2bfb80840
unknown <matthew.bootland@gmail.com>
2025-01-20 01:19:11 +0900

    Initialize Attributes from a Data Table

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -11,7 +11,7 @@ class UAbilitySystemComponent;
 class UAttributeSet;
 
 /**
- * 
+ *
  */
 UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
@@ -24,10 +24,9 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 protected:
-
-	UPROPERTY()
+	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
-	
+
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
 };
