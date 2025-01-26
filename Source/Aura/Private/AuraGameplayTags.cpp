f84a2cfe94a43a32d2ec490e15b94aaf3efc474f
unknown <matthew.bootland@gmail.com>
2025-01-27 07:31:03 +0900

    Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,11 @@
+// BootSoft
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+}
